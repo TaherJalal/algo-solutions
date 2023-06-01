da380b38effdfd467d2837fd042ec9517db0888b
TaherJalal <taherjalaljuma@gmail.com>
2023-06-01 10:33:11 +0300

    adding zeros to the right

---
+++ Leetcode/easy/moveZerosToTheLeft.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+         int index = 0;
+        for (int n : nums) {
+            if (n != 0) {
+                nums[index] = n;
+                index++;
+            }
+        }
+        while (index < nums.length) {
+            nums[index] = 0;
+            index++;
+        }
+    }
+}
