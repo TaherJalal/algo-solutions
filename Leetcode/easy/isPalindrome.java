5acb7fa62c64929e79fa229adfe50a2c05bc6c3f
Taher Jalal <99245750+TaherJalal@users.noreply.github.com>
2023-06-18 20:27:20 +0300

    Update isPalindrome.java

--- Leetcode/easy/isPalindrome.java
+++ Leetcode/easy/isPalindrome.java
@@ -18,4 +18,14 @@ public class Main {
         }
     }
 
+    // easier attempt
+    //    static boolean isPalindrome(String str) {
+//        String s = "";
+//        for (int i = str.length() - 1; i >= 0; i--) {
+//            s += str.charAt(i);
+//        }
+//        System.out.println(s);
+//        return str.equals(s);
+//    }
+
 }
