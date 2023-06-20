5b44aeb9ad0175472a843167378f34e42ee6656e
Taher Jalal <99245750+TaherJalal@users.noreply.github.com>
2023-06-20 13:29:18 +0300

    Update isDuplicate.java

--- Leetcode/easy/isDuplicate.java
+++ Leetcode/easy/isDuplicate.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<Integer>();
-        boolean ans = false;
         for(int n : nums){
             if(set.contains(n)){
-                ans = true;
-                break;
+                return true;
             }
             else{
                 set.add(n);
             }
         }
-        return ans;
+        return false;
     }
 }
